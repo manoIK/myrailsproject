0f295b7f876400950be45b75c7b66421b4b8292b
manoik <manoikgta@hotmail.com.com>
2020-11-24 23:54:58 +0200

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :friends
+  #get 'home/index'
+  # root 'home#index'
+
+  root 'friends#index'
+  get 'home/about'
+  get 'home/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
